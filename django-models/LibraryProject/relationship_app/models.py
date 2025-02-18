3f0e3410c9babfa40f7f46a86c5672d2b1debdb3
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-02-19 00:38:41 +0100

    task two b

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+# 1. ForeignKey Relationship
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    def __str__(self):
+        return self.title
+
+# 2. ManyToMany Relationship
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
+
+# 3. OneToOne Relationship
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(
+        Library,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='librarian'
+    )
+    
+    def __str__(self):
+        return self.name
