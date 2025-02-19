e0c76a63127e20e88f4b9caaccd2fbdfbea954ea
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-02-19 12:17:20 +0100

    task two still not worki

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view, list_books_view, LibraryDetailView 
+from .views import  list_books_view, home_view, LibraryDetailView 
 
 urlpatterns = [
     path("", home_view, name="home"),
