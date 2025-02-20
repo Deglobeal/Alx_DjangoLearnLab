1792dced050ca8f55485e4a76110ec9e7d354a64
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-02-20 08:09:36 +0100

    task three b

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,10 +4,14 @@ from .views import list_books
 from .views import admin_view, librarian_view, member_view
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import RegisterView
+from . import views
 
 
 
 urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
