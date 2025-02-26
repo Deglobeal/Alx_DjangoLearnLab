0617a33cc862e0dbfa77733e1b524e6314d6a483
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-02-26 15:33:08 +0100

    my template is almost ready

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -61,10 +61,10 @@ class Librarian(models.Model):
         related_name='librarian'
     )
     
-    class Book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    published_date = models.DateField()
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
