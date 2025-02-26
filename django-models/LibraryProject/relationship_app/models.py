c8f397924d5ed3569bc4c99fef882833da0c805e
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-02-26 15:28:48 +0100

    my template is almost ready

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -60,3 +60,33 @@ class Librarian(models.Model):
         primary_key=True,
         related_name='librarian'
     )
+    
+    class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        permissions = [
+            ("can_add_library", "Can add library"),
+            ("can_change_library", "Can change library"),
+            ("can_delete_library", "Can delete library"),
+        ]
