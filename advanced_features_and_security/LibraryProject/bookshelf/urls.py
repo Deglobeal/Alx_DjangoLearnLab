79890f8b8559d6791bed13d63c237d9adf0cc8fd
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-02 14:29:53 +0100

    working to improve settings

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import book_list
+from .views import example_form_view
 
 urlpatterns = [
     path('books/', book_list, name='book_list'),
+    path('example-form/', example_form_view, name='example_form'),
 ]
 
 
