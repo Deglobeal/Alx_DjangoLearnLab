8aca49cda14d277ec6eab15262c41fde62a684e4
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-06 14:31:39 +0100

    1st project working on model

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -10,3 +12,8 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Retrieve all books
     serializer_class = BookSerializer  # Use the BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access
