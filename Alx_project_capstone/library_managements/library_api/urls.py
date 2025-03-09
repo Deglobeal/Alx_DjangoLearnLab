7be66c7a59634f29ffc12788484f6c8f86522b8d
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-09 17:32:58 +0100

    worked on views

--- Alx_project_capstone/library_managements/library_api/urls.py
+++ Alx_project_capstone/library_managements/library_api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from library_managements.library_api import admin
 from .views import BookViewSet, UserViewSet, TransactionViewSet
 
-# Create a router and register the viewsets
+# Create a router and register the viewsets.
 router = DefaultRouter()
 router.register(r'books', BookViewSet)  # Register BookViewSet
 router.register(r'users', UserViewSet)  # Register UserViewSet
 router.register(r'transactions', TransactionViewSet)  # Register TransactionViewSet
 
-# Define the URL patterns
+# Define the URL patterns.
 urlpatterns = [
     path('', include(router.urls)),  # Include all routes from the router
     path('admin/', admin.site.urls),  # Django admin panel
