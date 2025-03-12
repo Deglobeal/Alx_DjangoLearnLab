dbd6f5f695de9410771d7d1bb7040b320a0dcb48
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-12 11:47:34 +0100

    working on api model 2

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -11,3 +11,11 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'publication_year', 'author', 'isbn', 'is_available']
+        
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'bio', 'books']
+
