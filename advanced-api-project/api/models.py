7014ccb98b7d9670585f30c84fef867d6b068221
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-12 11:22:51 +0100

    working on api model 1

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)  
+    bio = models.TextField(null=True, blank=True) 
+
+    def __str__(self):
+        return self.name
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)  
+    publication_year = models.PositiveIntegerField() 
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  
+    isbn = models.CharField(max_length=13, unique=True) 
+
+    def __str__(self):
+        return self.title
