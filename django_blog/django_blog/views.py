db852a8fbd00c4895bdbd3c38afd6dc0ee5284ac
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-20 22:38:18 +0100

    created forms.py to resolve the authentication

--- django_blog/django_blog/views.py
+++ django_blog/django_blog/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # from django.http import HttpResponse
 
 def homepage(request):
     # return HttpResponse("Hello world, i am improving on django")
     return render(request, 'base.html')
+
+
+class CustomLoginView(LoginView):
+    template_name = 'auth/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = 'login'
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register.html', {'form': form})
