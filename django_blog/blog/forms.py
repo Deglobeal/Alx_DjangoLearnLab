ff59c347d3a20de7117fd17456ed10ffa16fd061
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-20 08:22:53 +0100

    created forms.py to resolve the authentication

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,3 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from . forms import CustomUserCreationForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now login!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Error creating your account for you please try cheacking your form for mistakes')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Error updating your profile. Please check the form.')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'registration/profile.html', {'form': form})
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 app_name = 'blog'
@@ -8,5 +9,9 @@ app_name = 'blog'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
     
 ]
