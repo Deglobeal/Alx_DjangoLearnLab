d60c48898a64fdc1a3d85db1341518e4ea373459
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-20 22:26:27 +0100

    created forms.py to resolve the authentication

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -129,8 +129,9 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # ✅ Uses BASE_DIR to avoid hardcoding
+    BASE_DIR / "static",  # Global static directory (not app-specific)
 ]
 
 
