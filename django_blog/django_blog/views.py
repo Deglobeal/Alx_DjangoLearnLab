924bb3ada7da93c34e08711c4e9f50adea77540f
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-21 16:41:23 +0100

    working on template project

--- django_blog/django_blog/views.py
+++ django_blog/django_blog/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("hello")
+    #return HttpResponse("hello")
+    return render(request, 'base.html')
 
 
 def about(request):
-    return HttpResponse("about")
+    #return HttpResponse("about")
+    return render(request, 'about.html')
