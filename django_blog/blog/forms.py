36c22057e3888c188099123a8f57509af0fc03b7
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-23 06:49:17 +0100

    worked on create form

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'author')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Content'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+        }        
