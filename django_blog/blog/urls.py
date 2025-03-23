b9b1a1e76603b5616cec3382d1c2e60a583cba6a
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-23 19:33:35 +0100

    urls path update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,14 +8,14 @@ from .views import (
 
 urlpatterns = [
     # Post-related URLs
-    path('', HomeView.as_view(), name="home"),
     path('posts/', PostListView.as_view(), name='post_list'),
     path('posts/new/', PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     
-    # Authentication URLs
+    # Home page and authentication URLs
+    path('', HomeView.as_view(), name="home"),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', register, name='register'),
