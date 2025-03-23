1e72f640f9408ebd0e79e867431e0a48eeea3bb3
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-23 19:36:17 +0100

    urls path update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,10 +9,10 @@ from .views import (
 urlpatterns = [
     # Post-related URLs
     path('posts/', PostListView.as_view(), name='post_list'),
-    path('posts/new/', PostCreateView.as_view(), name='post_create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('post/new/', PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     
     # Home page and authentication URLs
     path('', HomeView.as_view(), name="home"),
