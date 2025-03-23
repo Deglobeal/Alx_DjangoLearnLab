9de045498c9f626e54f79c9ee109d2a67d14ecaa
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-23 06:09:52 +0100

    working on createView

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, PostDetailVew
+from .views import HomeView, PostDetailVew, AddPostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('post/<int:pk>', PostDetailVew.as_view(), name="post-detail"),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
 ]
