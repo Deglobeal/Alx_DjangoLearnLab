81d75bb075a37ac84ce5eaa60e6784e7b828e0d8
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-23 06:59:24 +0100

    working on updateView

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
-
-# Create your views here.
-#def home(request):
-#    return render(request, 'base.html', {})
 
 class HomeView(ListView):
     model = Post
@@ -21,3 +17,7 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update.html'
+    fields = ['title', 'content']
