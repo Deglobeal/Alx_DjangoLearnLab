7927047c2a00b54fdb3e7d5de0a947e09e38b18c
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-30 20:57:02 +0100

    fixed urls for tast 3

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,14 +1,25 @@
-from django.db import models
-from django.contrib.auth.models import User as CustomUser
-from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
-# Create your models here.
-class Notiification(models.Model):
-    recipient = models.ForeignKey(CustomUser, related_name='notifications', on_delete=models.CASCADE)
-    actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+class Notification(models.Model):  # Fixed typo in class name
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='notifications',
+        on_delete=models.CASCADE
+    )
+    actor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     verb = models.CharField(max_length=255)
-    target_ct = models.ForeignKey(ContentType, blank=True, null=True, on_delete=models.CASCADE)
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
     target_id = models.PositiveIntegerField(blank=True, null=True)
     target = GenericForeignKey('target_ct', 'target_id')
     read = models.BooleanField(default=False)
