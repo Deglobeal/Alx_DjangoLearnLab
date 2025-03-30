6d3ee66c93401a3307b531e28a71caae14a655fd
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-30 12:51:44 +0100

    fixed urls for tast two

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,28 +1,32 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from .models import CustomUser
 from .serializers import UserRegistrationSerializer, UserProfileSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = [permissions.AllowAny]
 
-    def create(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        
-        # Token creation happens in the view
-        token, created = Token.objects.get_or_create(user=user)
-        
+        token = Token.objects.create(user=user)
         return Response({
-            'user': serializer.data,
-            'token': token.key
-        }, status=status.HTTP_201_CREATED)
+            "user": serializer.data,
+            "token": token.key
+        }, status=201)
+
+class UserListView(generics.ListAPIView):  # New view for listing users
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()  # Explicit queryset declaration
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()  # Added explicit queryset
 
     def get_object(self):
         return self.request.user
