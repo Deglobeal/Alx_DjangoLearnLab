916c24ec841064f996714ccfcde19b9ea8754ef0
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-30 12:55:31 +0100

    fixed urls for tast two

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -30,3 +30,45 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    lookup_field = 'pk'
+
+    def post(self, request, *args, **kwargs):
+        user_to_follow = self.get_object()
+        current_user = request.user
+        
+        if current_user == user_to_follow:
+            return Response(
+                {"error": "You cannot follow yourself"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        current_user.following.add(user_to_follow)
+        return Response(
+            {"status": f"Now following {user_to_follow.username}"},
+            status=status.HTTP_200_OK
+        )
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    lookup_field = 'pk'
+
+    def delete(self, request, *args, **kwargs):
+        user_to_unfollow = self.get_object()
+        current_user = request.user
+        
+        if current_user == user_to_unfollow:
+            return Response(
+                {"error": "You cannot unfollow yourself"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        current_user.following.remove(user_to_unfollow)
+        return Response(
+            {"status": f"Unfollowed {user_to_unfollow.username}"},
+            status=status.HTTP_200_OK
+        )
