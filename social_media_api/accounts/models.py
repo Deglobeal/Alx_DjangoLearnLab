759655318c3a12ebc96cbdec5319a6e4e7e7f7e8
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-30 12:47:25 +0100

    fixed urls for tast two

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,8 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        blank=True,
+        null=True
+    )
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        blank=True,
+        related_name='following'
+    )
 
     def __str__(self):
         return self.username
