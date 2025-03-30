83dedc91bf485af6c66b6f4a98631968daec6fa3
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-30 12:37:09 +0100

    fixed serializer for tast two

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from accounts.models import CustomUser
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
